296b26fb5accb6553f1e7cc181ba9116f85016d9
Dan <danielpowell12@msn.com>
2020-12-10 16:57:04 -0700

    Adds test step to deploying

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders fallback page as default', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const fallbackElement = screen.getByText(/reached the profile site/i);
+  expect(fallbackElement).toBeInTheDocument();
 });
